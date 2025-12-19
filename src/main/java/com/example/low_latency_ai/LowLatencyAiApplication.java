553719b3701137e8d9118e925b07e14970686ab6
Cora Iberkleid <ciberkleid@gmail.com>
2025-12-19 10:39:26 -0500

    Initial commit

---
+++ src/main/java/com/example/low_latency_ai/LowLatencyAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.low_latency_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLatencyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLatencyAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/low_latency_ai/LowLatencyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.low_latency_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLatencyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
